41904b04e46ca6feee9297528ccd140bb131f7f7
garbagetown <garbagetown@gmail.com>
2015-12-16 00:50:25 +0900

    add spring security to secure the resource server

--- resource/src/main/java/com/example/ResourceApplication.groovy
+++ resource/src/main/java/com/example/ResourceApplication.groovy
@@ -2,16 +2,26 @@ package com.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
-class ResourceApplication {
+@EnableRedisHttpSession
+class ResourceApplication extends WebSecurityConfigurerAdapter {
 
     @RequestMapping('/')
     def home() {
         [id: UUID.randomUUID().toString(), content: 'Hello, World']
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+        http.authorizeRequests().anyRequest().authenticated()
     }
 
     static void main(String[] args) {

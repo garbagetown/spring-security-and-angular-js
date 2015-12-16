3f588581eb266c405a42304030dddadb3e13da47
garbagetown <garbagetown@gmail.com>
2015-12-16 23:24:13 +0900

    add the spring-security-oauth2 dependency

--- authserver/src/main/java/com/example/DemoApplication.java
+++ authserver/src/main/java/com/example/DemoApplication.java
@@ -2,9 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableAuthorizationServer
+public class DemoApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);

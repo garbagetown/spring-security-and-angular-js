b081cff072effe779d91f767a37427c45b82efc2
garbagetown <garbagetown@gmail.com>
2015-12-17 00:53:36 +0900

    modify resource server

--- authserver/src/main/java/com/example/DemoApplication.java
+++ authserver/src/main/java/com/example/DemoApplication.java
@@ -3,11 +3,23 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.security.Principal;
+
 @SpringBootApplication
+@RestController
 @EnableAuthorizationServer
+@EnableResourceServer
 public class DemoApplication extends WebMvcConfigurerAdapter {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);

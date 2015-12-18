4040c94e5786bd209fed0aec82e0ceeb86fa0fac
yuichiro umezawa <garbagetown@gmail.com>
2015-12-18 12:52:04 +0900

    remove the login route and method

--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -3,44 +3,24 @@ angular.module('hello', ['ngRoute'])
     $routeProvider.when('/', {
       templateUrl : 'home.html',
       controller : 'home'
-    }).when('/login', {
-      templateUrl : 'login.html',
-      controller : 'navigation'
     }).otherwise('/');
 
   })
   .controller('navigation', function($rootScope, $scope, $http, $location) {
 
-    var authenticate = function(credentials, callback) {
-      var headers = credentials ?
-        { authorization : "Basic " + btoa(credentials.username + ":" + credentials.password) } : {};
-      $http.get('user', {headers : headers}).success(function(data) {
-        if (data.name) {
-          $rootScope.authenticated = true;
-        } else {
-          $rootScope.authenticated = false;
-        }
-        callback && callback();
-      }).error(function() {
+    $http.get('user', {headers : headers}).success(function(data) {
+      if (data.name) {
+        $rootScope.authenticated = true;
+      } else {
         $rootScope.authenticated = false;
-        callback && callback();
-      });
-    }
-
-    authenticate();
+      }
+      callback && callback();
+    }).error(function() {
+      $rootScope.authenticated = false;
+      callback && callback();
+    });
 
     $scope.credentials = {};
-    $scope.login = function() {
-      authenticate($scope.credentials, function() {
-        if ($rootScope.authenticated) {
-          $location.path("/");
-          $scope.error = false;
-        } else {
-          $location.path("/login");
-          $scope.error = true;
-        }
-      });
-    };
 
     $scope.logout = function() {
       $http.post('logout', {}).success(function(data) {

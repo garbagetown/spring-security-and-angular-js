fb98801cc593d078b5d59085ea994ff498fdafd8
garbagetown <garbagetown@gmail.com>
2015-12-19 12:09:59 +0900

    modify the ui as a simple backend server

--- ui/src/main/java/com/example/UiApplication.java
+++ ui/src/main/java/com/example/UiApplication.java
@@ -2,36 +2,16 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableOAuth2Sso
+@EnableRedisHttpSession
 public class UiApplication extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests().antMatchers("/index.html", "home.html", "/").permitAll()
-            .anyRequest().authenticated()
-        .and()
-            .csrf().csrfTokenRepository(csrfTokenRepository())
-        .and()
-            .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
-        super.configure(http);
-    }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);

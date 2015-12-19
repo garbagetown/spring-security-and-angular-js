a0110633875f462b7575db3127affe9112a1c15b
garbagetown <garbagetown@gmail.com>
2015-12-19 19:52:51 +0900

    roll back to the 'basic' version

--- ui/src/main/java/com/example/UiApplication.java
+++ ui/src/main/java/com/example/UiApplication.java
@@ -2,26 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @RestController
 @EnableRedisHttpSession
-public class UiApplication extends WebSecurityConfigurerAdapter {
+public class UiApplication {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .httpBasic()
-        .and()
-            .authorizeRequests().antMatchers("/index.html", "/").permitAll()
-        .anyRequest().hasRole("USER");
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
     }
 
     public static void main(String[] args) {
--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -1,36 +1,5 @@
-angular.module('hello', ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider.when('/', {
-      templateUrl : 'home.html',
-      controller : 'home'
-    }).otherwise('/');
-
-  })
-  .controller('navigation', function($rootScope, $scope, $http, $location) {
-
-    $http.get('user').success(function(data) {
-      if (data.name) {
-        $rootScope.authenticated = true;
-      } else {
-        $rootScope.authenticated = false;
-      }
-    }).error(function() {
-      $rootScope.authenticated = false;
-    });
-
-    $scope.credentials = {};
-
-    $scope.logout = function() {
-      $http.post('logout', {}).success(function(data) {
-        $rootScope.authenticated = false;
-        $location.path("/");
-      }).error(function(data) {
-        $rootScope.authenticated = false;
-      });
-    }
-  })
-  .controller('home', function($scope, $http) {
-    $http.get('resource/').success(function(data) {
-      $scope.greeting = data;
-    });
-  });
+angular.module('hello', []).controller('home', function($scope, $http) {
+	$http.get('/resource/').success(function(data) {
+		$scope.greeting = data;
+	})
+});

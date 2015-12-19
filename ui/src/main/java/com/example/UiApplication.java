aafaaf0c791ca53d07ea0fa6b4ff8b0bd8ae7d58
garbagetown <garbagetown@gmail.com>
2015-12-19 13:58:06 +0900

    add security settings

--- ui/src/main/java/com/example/UiApplication.java
+++ ui/src/main/java/com/example/UiApplication.java
@@ -8,10 +8,21 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableRedisHttpSession
 public class UiApplication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic()
+        .and()
+            .authorizeRequests().antMatchers("/index.html", "/").permitAll()
+        .anyRequest().hasRole("USER");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);

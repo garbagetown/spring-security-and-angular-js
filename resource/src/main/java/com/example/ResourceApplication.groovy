850a30cdafd819e4f8e8176f6d9761bcaefaf9c1
garbagetown <garbagetown@gmail.com>
2015-12-19 12:20:40 +0900

    modify the resource project to use redis http session

--- resource/src/main/java/com/example/ResourceApplication.groovy
+++ resource/src/main/java/com/example/ResourceApplication.groovy
@@ -2,13 +2,13 @@ package com.example
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
-@EnableResourceServer
+@EnableRedisHttpSession
 class ResourceApplication {
 
     @RequestMapping('/')

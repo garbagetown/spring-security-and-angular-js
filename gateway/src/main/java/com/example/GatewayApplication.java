c96b0599aa3d0fac2bed07f37fec470757436938
garbagetown <garbagetown@gmail.com>
2015-12-21 00:57:45 +0900

    add ADMIN role

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -26,12 +28,21 @@ public class GatewayApplication extends WebSecurityConfigurerAdapter {
         return map;
     }
 
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
+                .and().withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
+                .and().withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .httpBasic()
         .and()
-            .authorizeRequests().antMatchers("/index.html", "/login", "/").permitAll()
+            .authorizeRequests()
+                .antMatchers("/index.html", "/login", "/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
             .anyRequest().authenticated();
     }
 

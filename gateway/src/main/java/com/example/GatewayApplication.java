b9c9e798179ed0cceeac66e2eb2a621bd70945bf
garbagetown <garbagetown@gmail.com>
2015-12-29 11:36:47 +0900

    modify the authentication method and configurations

--- gateway/src/main/java/com/example/GatewayApplication.java
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -3,10 +3,15 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,31 +24,42 @@ import java.util.Map;
 @EnableRedisHttpSession
 @EnableZuulProxy
 @RestController
-public class GatewayApplication extends WebSecurityConfigurerAdapter {
+public class GatewayApplication {
 
     @RequestMapping("/user")
     public Map<String, Object> user(Principal user) {
         Map<String, Object> map = new LinkedHashMap<>();
-        map.put("name", "user");
+        map.put("name", user.getName());
+        map.put("roles", AuthorityUtils.authorityListToSet(((Authentication) user).getAuthorities()));
         return map;
     }
 
-    @Autowired
-    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
-                .and().withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
-                .and().withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
-    }
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .httpBasic()
-        .and()
-            .authorizeRequests()
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+            .and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN", "READER", "WRITER")
+            .and()
+                .withUser("audit").password("audit").roles("USER", "ADMIN", "READER");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .httpBasic()
+            .and()
+                .logout()
+            .and()
+                .authorizeRequests()
                 .antMatchers("/index.html", "/login", "/").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-            .anyRequest().authenticated();
+                .anyRequest().authenticated();
+        }
     }
 
     public static void main(String[] args) {

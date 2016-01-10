fd98ee70fd16d97cf2228518486f0ef433e943f2
garbagetown <garbagetown@gmail.com>
2016-01-10 15:11:00 +0900

    add a missed annotation

--- ui/src/main/java/com/example/UiApplication.java
+++ ui/src/main/java/com/example/UiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 
@@ -17,6 +18,7 @@ public class UiApplication {
     }
 
     @RequestMapping("/user")
+    @ResponseBody
     public Principal user(Principal user) {
         return user;
     }
